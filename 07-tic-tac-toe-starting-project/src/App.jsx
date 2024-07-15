ac983fc303d6e065ccde3cbbceda9c4425b06f43
kevit-gungun-udhani <gungun.udhani@kevit.io>
2024-07-15 16:14:40 +0530

    game bord created

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
+function App() {
+  
+
+  return (
+   <main>
+      {/*players*/}
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player-1" symbol="X"/>
+          <Player name="Player-2" symbol="O"/>
+        </ol>
+        <GameBoard/>
+      </div>
+      
+   </main>
+  )
+}
+
+export default App
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export default function Player({name, symbol}){
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(name);
+    function handleEditClick(){
+        setIsEditing((editing) => !editing );
+    }
+
+    function handleChange(event){
+        console.log(event)
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    if(isEditing){
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+    }
+
+    return (
+        <li>
+           <span className="player">
+            {editablePlayerName}
+            <span className="player-symbol">{symbol}</span>
+           </span>
+           <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
